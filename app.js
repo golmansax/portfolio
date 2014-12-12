8b6bbc320dd7cf339b238a9b1aae381b53ea352f
Holman Gao <holman@golmansax.com>
2014-12-11 16:02:54 -0800

    Port config

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var expressHandlebars = require('express-handlebars');
 
+app.listen(process.env.PORT || 3000);
+
 app.engine('hbs', expressHandlebars());
 app.set('view engine', 'hbs');
 
