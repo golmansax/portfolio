0d5760016d41b232370389ef9c304ef3d8db9230
Holman Gao <holman@golmansax.com>
2014-12-11 22:29:26 -0800

    Modifying jscsrc

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var expressHandlebars = require('express-handlebars');
 var hbs = expressHandlebars.create({
   extname: '.hbs',
   helpers: {
-    '__': function() {
+    '__': function () {
       return i18n.__.apply(this, arguments);
     }
   }
@@ -24,10 +24,10 @@ var hbs = expressHandlebars.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/assets/main.css', function(request, response) {
+app.get('/assets/main.css', function (request, response) {
   response.sendfile('./public/main.css');
 });
