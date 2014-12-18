7fd1d66a26cdde62a164b79224efa6cb3b3e8386
Holman Gao <holman@golmansax.com>
2014-12-18 10:22:00 -0800

    Adding jshint

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-var express = require('express');
-var app = express();
+(function () {
+  'use strict';
 
-var i18n = require('i18n');
+  var express = require('express');
+  var app = express();
 
-app.listen(process.env.PORT || 3000);
+  var i18n = require('i18n');
 
-i18n.configure({
-  directory: __dirname + '/locales',
-  objectNotation: true
-});
-app.use(i18n.init);
+  app.listen(process.env.PORT || 3000);
 
-var expressHandlebars = require('express-handlebars');
-var hbs = expressHandlebars.create({
-  extname: '.hbs',
-  helpers: {
-    '__': function () {
-      return i18n.__.apply(this, arguments);
+  i18n.configure({
+    directory: __dirname + '/locales',
+    objectNotation: true
+  });
+  app.use(i18n.init);
+
+  var expressHandlebars = require('express-handlebars');
+  var hbs = expressHandlebars.create({
+    extname: '.hbs',
+    helpers: {
+      '__': function () {
+        return i18n.__.apply(this, arguments);
+      }
     }
-  }
-});
+  });
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
+  app.engine('hbs', hbs.engine);
+  app.set('view engine', 'hbs');
 
-var stylus = require('stylus');
-var nib = require('nib');
-app.use(stylus.middleware({
-  src: __dirname + '/stylesheets',
-  dest: __dirname + '/public/assets',
-  compile: function compile(str, path) {
-    return stylus(str).set('filename', path).use(nib());
-  }
-}))
+  var stylus = require('stylus');
+  var nib = require('nib');
+  app.use(stylus.middleware({
+    src: __dirname + '/stylesheets',
+    dest: __dirname + '/public/assets',
+    compile: function compile(str, path) {
+      return stylus(str).set('filename', path).use(nib());
+    }
+  }));
 
-app.use(express.static(__dirname + '/public'))
+  app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+  app.get('/', function (req, res) {
+    res.render('index');
+  });
+})();
