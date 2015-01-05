471e2164c075106af2c408d72857f0502b47f0be
Holman Gao <holman@golmansax.com>
2015-01-04 21:40:01 -0800

    Fix loading issue

--- assets/main.jsx
+++ assets/main.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 var Resume = require('../components/resume.jsx');
 var gon = require('./gon');
 
-React.render(
-  <Resume entries={gon.resumeEntries} />,
-  window.document.getElementById('react-container')
-);
+window.document.addEventListener('DOMContentLoaded', function (){
+  'use strict';
+
+  React.render(
+    <Resume entries={gon.resumeEntries} />,
+    window.document.getElementById('react-container')
+  );
+});
