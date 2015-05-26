2ca78269056be74ffdf1bf522beaf927f3aac1f6
Holman Gao <holman@golmansax.com>
2015-05-26 16:04:19 -0700

    Donations to es6

--- jsx/donations.jsx
+++ jsx/donations.jsx
@@ -3,23 +3,27 @@
 var React = require('react');
 var Fragment = require('./fragment');
 
-module.exports = React.createClass({
-  getInitialProps: function () {
-    return { donations: [] };
-  },
+class Donations extends React.Component {
+  constructor(props) {
+    super(props);
+    this._renderDonation = this._renderDonation.bind(this);
+  }
 
-  _renderDonation: function (donation) {
+  _renderDonation(donation) {
     return (
       <div key={donation.year}>
         {donation.year} –&nbsp;
         <Fragment {...donation.organization} />
       </div>
     );
-  },
+  }
 
-  render: function () {
+  render() {
     var donations = this.props.donations.map(this._renderDonation);
 
     return <div className='container'>{donations}</div>;
   }
-});
+}
+Donations.defaultProps = { donations: [] };
+
+module.exports = Donations;
