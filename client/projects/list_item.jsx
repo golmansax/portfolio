66173fd3fa83bc67012d76c5474608002cb19605
Holman Gao <holman@golmansax.com>
2015-06-01 13:24:53 -0700

    try again

--- client/projects/list_item.jsx
+++ client/projects/list_item.jsx
@@ -19,6 +19,8 @@ export default class ProjectsListItem extends React.Component {
           <div className='projects-list-item-right'>
             <h2><a href={this.props.url}>{this.props.name}</a></h2>
             <dl>
+              <dt>Product:</dt>
+              <dd>An online form management tool for schools</dd>
               <dt>Position:</dt>
               <dd>Software Engineer (May - 2014)</dd>
               <dt>Stack:</dt>
@@ -27,10 +29,23 @@ export default class ProjectsListItem extends React.Component {
               <dd>Team had 3 employees (2 engineers)</dd>
               <dt>Involved with:</dt>
               <dd>
-                Helped create the engineering culture as an early employee
-                Practice agile development through full code coverage and strong integration tests
-                Focus on customer satisfaction through integrating user feedback and fast response times to support requests
-                Comfortable with full stack of website development, from code architecting to frontend design
+                <ul>
+                  <li>
+                    Helped create the engineering culture as an early employee
+                  </li>
+                  <li>
+                    Practice agile development through full code coverage and
+                    strong integration tests
+                  </li>
+                  <li>
+                    Focus on customer satisfaction through integrating user
+                    feedback and fast response times to support requests
+                  </li>
+                  <li>
+                    Comfortable with full stack of website development, from
+                    code architecting to frontend design
+                  </li>
+                </ul>
               </dd>
             </dl>
           </div>
