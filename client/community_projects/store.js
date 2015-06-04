40fb437424f993cda4720838d7d69bfda06a0916
Holman Gao <holman@golmansax.com>
2015-06-04 14:37:29 -0700

    Update jscs

--- client/community_projects/store.js
+++ client/community_projects/store.js
@@ -5,7 +5,7 @@ import i18n from 'i18next';
 import ProjectsArray from '../projects/array';
 
 export function getAll() {
-  var projects = ProjectsArray.from(i18n.t('community_projects:communityProjects'))
+  var projects = ProjectsArray.from(i18n.t('community_projects:projects'))
     .map(function (project) {
       Object.assign(project, { projectPath: `/in-community#${project.slug}` });
       return project;
