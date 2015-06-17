e5e94cc556f532e50f20250460510c981cd58de7
Holman Gao <holman@golmansax.com>
2015-06-16 19:36:10 -0700

    Remove sidebar

--- client/content/handler.jsx
+++ client/content/handler.jsx
@@ -3,17 +3,16 @@ import { RouteHandler } from 'react-router';
 import DocumentTitle from 'react-document-title';
 import ContentNavbar from './navbar';
 import ContentFooter from './footer';
-import ContentSidebarContainer from './sidebar_container';
 
 export default class ContentHandler extends React.Component {
   render() {
     return (
       <DocumentTitle title='Holman Gao'>
-        <ContentSidebarContainer>
+        <div>
           <ContentNavbar />
           <RouteHandler />
           <ContentFooter />
-        </ContentSidebarContainer>
+        </div>
       </DocumentTitle>
     );
   }
