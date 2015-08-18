75a85b060202fcbe0b240d424cf95782f3a843dd
Holman Gao <holman@golmansax.com>
2015-08-18 13:34:02 -0700

    Move pdf styl into its own file

--- client/shared/pdf.jsx
+++ client/shared/pdf.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Pdf extends React.Component {
   render() {
-    return <div className='pdf-viewer'><iframe src={this.props.url} /></div>;
+    return <div className='gsax-pdf'><iframe src={this.props.url} /></div>;
   }
 }
 Pdf.propTypes = { url: React.PropTypes.string.isRequired };
