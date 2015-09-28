9195942c37c5f8cb64da34a06aaf7e29fa0b185b
Holman Gao <holman@golmansax.com>
2015-09-28 16:10:40 -0700

    Broke zombie tests

--- test/server_test.js
+++ test/server_test.js
@@ -15,83 +15,63 @@ describe('server', function() {
     this.timeout(9000);
     this.server = server.listen(port);
     browser = new Browser({ site: 'http://localhost:' + port });
+    return browser.visit('/');
   });
 
   afterEach(() => {
     this.server.close();
   });
 
-  it('routes root page to portfolio', (done) => {
-    browser.visit('/', () => {
-      expect(browser.text('title')).to.equal('Holman Gao');
-      expect(browser.text('h1')).to.equal('Holman Gao');
-      expect(browser.text('body')).to.include('Chalk Schools');
-      StinkBomb.create('2016-06-01', { message: 'Sample stink bomb' });
-      done();
-    });
+  it('routes root page to portfolio', () => {
+    expect(browser.text('title')).to.equal('Holman Gao');
+    expect(browser.text('h1')).to.equal('Holman Gao');
+    expect(browser.text('body')).to.include('Chalk Schools');
+    StinkBomb.create('2016-06-01', { message: 'Sample stink bomb' });
   });
 
   it('can click to resume', (done) => {
-    browser.visit('/', () => {
-      browser.clickLink('Resume', () => {
-        expect(browser.text('title')).to.include('Resume');
-        done();
-      });
-    });
+    browser.clickLink('Resume').then(() => {
+      expect(browser.text('title')).to.include('Resume');
+    }).then(done);
   });
 
   it('can click on a portfolio link', (done) => {
-    browser.visit('/', () => {
-      browser.clickLink('Chalk Schools', () => {
-        expect(browser.text('title')).to.include('Chalk Schools');
-        expect(browser.text('body')).to.include('A digital workflow system');
-        done();
-      });
-    });
+    browser.clickLink('Chalk Schools').then(() => {
+      expect(browser.text('title')).to.include('Chalk Schools');
+      expect(browser.text('body')).to.include('A digital workflow system');
+    }).then(done);
   });
 
   it('can click on a header link', (done) => {
-    browser.visit('/', () => {
-      browser.clickLink('Side Projects', () => {
-        expect(browser.text('title')).to.include('Side Projects');
-        expect(browser.text('body')).to.include('LAUNCH Hackathon 2015');
-        expect(browser.text('body')).to.include('Internal Room 77 hackathon');
-        done();
-      });
-    });
+    browser.clickLink('Side Projects', () => {
+      expect(browser.text('title')).to.include('Side Projects');
+      expect(browser.text('body')).to.include('LAUNCH Hackathon 2015');
+      expect(browser.text('body')).to.include('Internal Room 77 hackathon');
+    }).then(done);
   });
 
   it('can click to hcd page', (done) => {
-    browser.visit('/', () => {
-      browser.clickLink('Resume', () => {
-        browser.clickLink('human-centered design', () => {
-          expect(browser.text('title')).to.include('Human-Centered Design');
-          done();
-        });
-      });
+    browser.clickLink('Resume', () => {
+      browser.clickLink('human-centered design', () => {
+        expect(browser.text('title')).to.include('Human-Centered Design');
+      }).then(done);
     });
   });
 
   it('can click to donations page', (done) => {
-    browser.visit('/', () => {
-      browser.clickLink('Resume', () => {
-        browser.clickLink('my donations pledge', () => {
-          expect(browser.text('title')).to.include('Donations Pledge');
-          done();
-        });
-      });
+    browser.clickLink('Resume', () => {
+      browser.clickLink('my donations pledge', () => {
+        expect(browser.text('title')).to.include('Donations Pledge');
+      }).then(done);
     });
   });
 
   it('can click to street view', (done) => {
-    browser.visit('/', () => {
-      browser.clickLink('Resume', () => {
-        browser.clickLink('More');
-        browser.clickLink('Fun teams page', () => {
-          expect(browser.text('title')).to.include('Office Street View');
-          done();
-        });
-      });
+    browser.clickLink('Resume', () => {
+      browser.clickLink('More');
+      browser.clickLink('Fun teams page', () => {
+        expect(browser.text('title')).to.include('Office Street View');
+      }).then(done);
     });
   });
 });
