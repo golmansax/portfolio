1e2aa40cf1326f359b2cd409e94c351d82c07d29
Holman Gao <holman@golmansax.com>
2015-11-24 19:02:59 -0800

    See if gh deploy works!

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -3,3 +3,9 @@
 cd public
 git init
 
+git config user.name "Holman Gao"
+git config user.email "holman@golmansax.com"
+
+git add .
+git commit -m 'Deploy to Github Pages'
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
