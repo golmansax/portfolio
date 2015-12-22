d8dab7cc12ba80a452350e22b7518878266090e1
Holman Gao <holman@golmansax.com>
2015-12-22 15:04:34 -0800

    Add title to 404

--- client/routes/not_found_handler.jsx
+++ client/routes/not_found_handler.jsx
@@ -1,7 +1,9 @@
+import Helmet from 'react-helmet';
 import Container from '../shared/container';
 
 const NotFoundHandler = () => (
   <Container>
+    <Helmet title='Page not found' />
     <h1 className='not-found-handler__message'>
       We can&rsquo;t find the page you&rsquo;re looking for
     </h1>
