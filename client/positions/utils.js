8e41692a2ac5394a0a23a7c6becc8df5c6ca0fbb
Holman Gao <holman@golmansax.com>
2016-02-23 00:40:58 +0300

    Add zenysis

--- client/positions/utils.js
+++ client/positions/utils.js
@@ -1,3 +1,8 @@
+const getDateText = (start, end) => {
+  if (start === end) { return start };
+  return `${start} – ${end}`;
+};
+
 export function getPositionText({ title, start, end }) {
-  return `${title} (${start} – ${end})`;
+  return `${title} (${getDateText(start, end)})`;
 }
