1da2a6da83abd0c3233e7d94750bc0f479141ae0
Holman Gao <holman@golmansax.com>
2016-02-24 09:47:53 -0800

    Make outside links open new tab

--- client/fragments/fragment.jsx
+++ client/fragments/fragment.jsx
@@ -8,7 +8,11 @@ const Fragment = ({ routeName, text, github, url }) => {
   } else if (github) {
     return <GithubFragment github={github} />;
   } else if (url) {
-    return <a href={url}>{text || url}</a>;
+    return (
+      <a href={url} target={url.match(/https?:/) ? '_blank': null}>
+        {text || url}
+      </a>
+    );
   }
 
   return <span>{text}</span>;
