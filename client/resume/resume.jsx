3689035a1552321881f98de8c5b46d63f2809b40
Holman Gao <holman@golmansax.com>
2016-08-26 18:06:36 -0400

    Update resume link

--- client/resume/resume.jsx
+++ client/resume/resume.jsx
@@ -3,7 +3,7 @@ import Container from '../shared/container';
 import Pdf from '../shared/pdf';
 
 const URL = 'https://docs.google.com/viewer?' +
-  'srcid=0BzCKqkqhnFJQNkJibEduXzZDYm8&pid=explorer&efh=false' +
+  'srcid=0BzLh8cg8kVzCV3QzMTdVVmtqUWs&pid=explorer&efh=false' +
   '&a=v&chrome=false&embedded=true';
 
 const Resume = () => (
