80e664a171286279828e2bb62781a8d5b3c8e881
Holman Gao <holman@golmansax.com>
2016-09-29 18:38:08 -0400

    Update server to be from port 3010

--- server/config.js
+++ server/config.js
@@ -4,4 +4,4 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const isDevelopment = () => NODE_ENV === 'development';
 export const isProduction = () => NODE_ENV === 'production';
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3010;
