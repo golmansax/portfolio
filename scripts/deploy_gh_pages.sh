2e201b2d7a8e1275627f0dda69cf78da9291e62e
Holman Gao <holman@golmansax.com>
2016-11-18 23:04:18 -0500

    Don't deploy CNAME to portfolio

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -9,4 +9,7 @@ git config user.email "holman@golmansax.com"
 git add .
 git commit -m 'Deploy to Github Pages'
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
+
+git rm CNAME
+git commit -m 'Remove CNAME'
 git push --force --quiet "https://${GH_TOKEN}@github.com/golmansax/portfolio.git" master:gh-pages
