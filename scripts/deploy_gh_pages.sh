c25d13b09b2045a5f79ed686e3f11cc0d56309f5
Holman Gao <holman@golmansax.com>
2016-11-26 19:15:50 -0500

    Don't deploy to gh-pages branch just yet

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -10,6 +10,6 @@ git add .
 git commit -m 'Deploy to Github Pages'
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
 
-git rm CNAME
-git commit -m 'Remove CNAME'
-git push --force --quiet "https://${GH_TOKEN}@github.com/golmansax/portfolio.git" master:gh-pages
+# git rm CNAME
+# git commit -m 'Remove CNAME'
+# git push --force --quiet "https://${GH_TOKEN}@github.com/golmansax/portfolio.git" master:gh-pages
