7fae2d69e4f98ee8173dbf0b9f5740fb17cd8057
Holman Gao <holman@golmansax.com>
2016-12-09 00:39:10 -0500

    Add mp3 to cache list

--- server/asset_utils.js
+++ server/asset_utils.js
@@ -3,5 +3,7 @@ import cachifyStatic, { cachify } from 'connect-cachify-static';
 
 export const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
-const middleware = cachifyStatic(PUBLIC_DIR);
+const middleware = cachifyStatic(PUBLIC_DIR, {
+  match: /\.(js|css|svg|eot|woff|ttf|cur|png|gif|jpg|mp3)$/,
+});
 export { middleware as assetMiddleware, cachify as getAsset };
