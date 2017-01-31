4447b5ec796cf7c6506f88fed082186bf41a9584
Holman Gao <holman@golmansax.com>
2017-01-31 01:54:46 -0500

    Redo footer links

--- client/content/footer/footer.jsx
+++ client/content/footer/footer.jsx
@@ -1,4 +1,3 @@
-import ContentFooterInfoTags from './info_tags';
 import ContentMainLinks from '../main_links';
 import Container from '../../shared/container';
 import MyEmail from '../../my/email';
@@ -16,12 +15,26 @@ const ContentFooter = () => (
     </div>
     <div className='content-footer__bottom'>
       <Container>
+        <div className='content-footer__large-section'>
+          <h2>
+            Holman Gao <small>© {new Date().getFullYear()}</small>
+          </h2>
+          <MyEmail link />
+          <div>in New York, NY</div>
+        </div>
         <div className='content-footer__section'>
-          <h2>Holman Gao</h2>
+          <div className='content-footer__section-heading'>Portfolio</div>
           <ContentMainLinks className='content-footer__main-links' />
         </div>
         <div className='content-footer__section'>
-          <ContentFooterInfoTags />
+          <div className='content-footer__section-heading'>Other links</div>
+          <div className='content-footer__main-links'>
+            <a href='/'>Home</a>
+            <a href='/blog'>Blog</a>
+            <a href='/resume'>Resume</a>
+            <a href='https://github.com/golmansax'>GitHub</a>
+            <a href='https://twitter.com/golmansax'>Twitter</a>
+          </div>
         </div>
       </Container>
     </div>
