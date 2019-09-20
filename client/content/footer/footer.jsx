55e7e62e044b0227f1a88910e4de71474c02de2d
Holman Gao <holman@golmansax.com>
2019-09-20 13:28:21 -0400

    copy: Tweak copy in footer

--- client/content/footer/footer.jsx
+++ client/content/footer/footer.jsx
@@ -19,7 +19,7 @@ const ContentFooter = () => (
           <div className='content-footer__section-heading'>
             <Fragment
               routeName={getPortfolioPath('/work-together')}
-              text='Need a Freelancer?'
+              text='Need Help With Early-Stage Tech Decisions?'
             />
           </div>
           <br />
