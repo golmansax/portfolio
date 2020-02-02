676c706cf0d4c15e6b608cfd1f148e84f4002d8e
Holman Gao <holman@golmansax.com>
2020-02-01 21:51:54 -0500

    feat: Add LinkedIn

--- client/content/footer/footer.jsx
+++ client/content/footer/footer.jsx
@@ -42,6 +42,9 @@ const ContentFooter = () => (
             <a href='https://github.com/golmansax' target='_blank' rel='noopener noreferrer'>
               GitHub
             </a>
+            <a href='https://www.linkedin.com/in/holman-gao/' target='_blank' rel='noopener noreferrer'>
+              LinkedIn
+            </a>
             <a href='https://twitter.com/golmansax' target='_blank' rel='noopener noreferrer'>
               Twitter
             </a>
